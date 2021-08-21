615b961fba4fbc2960f8ba4bfbb85025265304b6
Jatin More <54055686+jatinmore@users.noreply.github.com>
2021-08-22 01:19:44 +0530

    final commit

--- app.js
+++ app.js
@@ -2,40 +2,48 @@ var billAmt=document.querySelector('#Bill-Amt');
 var cashGiven=document.querySelector('#Cash-given');
 var checkBtn=document.querySelector('#btn-check');
 var errorMessage=document.querySelector('#error-msg');
+var noOfNote=document.querySelector('.no-of-notes')
 
-console.log(billAmt.value);
+const notes =[2000,500,200,100,50,20,10,5,1];
 checkBtn.addEventListener('click',function validateCash(){
- 
+    hideMsg();
     if(billAmt.value>0)
     {
-        if(cashGiven.value>=billAmt.value)
+        if(cashGiven.value >= billAmt.value)
         {
-            
             var calculateCash=cashGiven.value - billAmt.value;
             validateChange(calculateCash);
         }
         else
         {
-            showError('wanna wash plates?')
+            showError('wanna wash plates?🍽️');
         }
     }
     else{
     
-        showError('Invalid Amount')
+        showError('Invalid Amount');
     }
 
-})
+});
 
 function hideMsg()
 {
-    errorMessage.style.display='None';
+    errorMessage.style.display='none';
 }
-function validateChange()
+function validateChange(calculateCash)
 {
-
+    
+    for (let i = 0;i<notes.length;i++)
+    {
+        const calcNote=Math.trunc(
+        calculateCash/notes[i]
+        );
+    calculateCash=calculateCash%notes[i];
+    noOfNote[i].innerText=calcNote;
+    }
 }
 function showError(errorMsg)
 {
-    errorMessage.style.display='Block';
+    errorMessage.style.display='block';
     errorMessage.innerText=errorMsg;
 }
