16a95c6633e7e9519a16386ddba0b12c5f63d0e2
kpavann <kulkarnipavan6115@gmail.com>
2021-08-22 15:35:33 +0530

    updated logic

--- app.js
+++ app.js
@@ -1,49 +1,43 @@
-var billAmt=document.querySelector('#Bill-Amt');
-var cashGiven=document.querySelector('#Cash-given');
-var checkBtn=document.querySelector('#btn-check');
-var errorMessage=document.querySelector('#error-msg');
-var noOfNote=document.querySelector('.no-of-notes')
+var billAmt = document.querySelector('#Bill-Amt');
+var cashGiven = document.querySelector('#Cash-given');
+var checkBtn = document.querySelector('#btn-check');
+var errorMessage = document.querySelector('#error-msg');
+var noOfNote = document.querySelectorAll('.no-of-notes')
 
-const notes =[2000,500,200,100,50,20,10,5,1];
-checkBtn.addEventListener('click',function validateCash(){
+const notes = [2000, 500, 100, 20, 10, 5, 1];
+
+checkBtn.addEventListener('click', function validateCash() {
     hideMsg();
-    if(billAmt.value>0)
-    {
-        if(cashGiven.value >= billAmt.value)
-        {
-            var calculateCash=cashGiven.value - billAmt.value;
-            validateChange(calculateCash);
-        }
-        else
-        {
-            showError('wanna wash plates?🍽️');
-        }
+    if (billAmt.value > 0) {
+        checkCashGiven()
+    } 
+    else {
+        showError('Bill amount less than zero, please check');
     }
-    else{
-    
-        showError('Invalid Amount');
-    }
-
 });
 
-function hideMsg()
-{
-    errorMessage.style.display='none';
-}
-function validateChange(calculateCash)
-{
-    
-    for (let i = 0;i<notes.length;i++)
-    {
-        const calcNote=Math.trunc(
-        calculateCash/notes[i]
-        );
-    calculateCash=calculateCash%notes[i];
-    noOfNote[i].innerText=calcNote;
+function checkCashGiven() {
+    if (cashGiven.value >= billAmt.value) {
+        var calculateCash = cashGiven.value - billAmt.value;
+        validateChange(calculateCash);
+    } else {
+        showError('Cash given is less than the bill amount');
     }
 }
-function showError(errorMsg)
-{
-    errorMessage.style.display='block';
-    errorMessage.innerText=errorMsg;
+
+function validateChange(calculateCash) {
+    for (let i = 0; i < notes.length; i++) {
+        const calcNote = Math.trunc(calculateCash / notes[i]);
+        calculateCash %= notes[i];
+        noOfNote[i].innerText = calcNote;
+    }
+}
+
+function hideMsg() {
+    errorMessage.style.display = 'none';
+}
+
+function showError(errorMsg) {
+    errorMessage.style.display = 'block';
+    errorMessage.innerText = errorMsg;
 }
